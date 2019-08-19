0807a552680a6091256c42021dfe831e21e698d9
Zhijie Wang <wying256257@gmail.com>
2019-08-19 17:21:40 +0800

    Add Post field

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=512)
+    article =  models.TextField(blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
