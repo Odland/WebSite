b63e0ebe503582ed533663e817d1f17333b7e896
Zhijie Wang <wying256257@gmail.com>
2019-10-14 21:24:29 +0800

    Modify the app name Home to Blog

--- Blog/models.py
+++ Blog/models.py
@@ -21,10 +21,3 @@ class Blog(models.Model):
     def __str__(self):
         return "<Blog: %s>" % self.title # 显示标题
 
-
-
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=512)
-    article =  models.TextField(blank=True)
-    pub_date = models.DateTimeField(auto_now_add=True)
