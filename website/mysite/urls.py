8889c7a86a743952e9bc1d9bb091337317b29ba7
Zhijie Wang <wying256257@gmail.com>
2021-07-20 00:02:02 +0800

    [Add]Add a home page

---
+++ website/mysite/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('home/',views.index,name='index')
+]
--- website/mysite/views.py
+++ website/mysite/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("<H1 align='center'>This is a home page</H1>")
